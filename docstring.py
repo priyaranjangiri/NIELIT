5a604191637bb5da9ca23adbae4b8f816d285e54
bagad-billa420 <priyaranjangiri006@gmail.com>
2026-04-16 07:19:01 +0530

    string operation

---
+++ docstring.py
@@ -0,0 +1,4 @@
+#eval performs the multiplication passed as arguments
+number=10
+sq_num= eval('number*number')
+print(sq_num)
---
+++ string.py
@@ -0,0 +1,20 @@
+my_string = "Hello, World!"
+print(my_string.capitalize())  # Output: "Hello, world!"
+print(my_string.upper())       # Output: "HELLO, WORLD!"
+print(my_string.lower())       # Output: "hello, world!"
+print(my_string.replace("World", "Python"))  # Output: "Hello, Python!"
+print(my_string.split(", "))  # Output: ['Hello', 'World!']
+print(my_string.strip())       # Output: "Hello, World!"
+
+str="BALAJEE"
+print(str.isupper())
+print(str.islower())
+print(str.istitle())
+str1="nielt"
+print(str1.isupper())
+print(str1.islower())
+print(str1.istitle()) 
+str2="Good"
+print(str2.isupper())   
+print(str2.islower())
+print(str2.istitle())
