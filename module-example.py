d2b6c144cbf1efeb3270ee199a526dc8253491d4
bagad-billa420 <priyaranjangiri006@gmail.com>
2026-04-17 05:34:21 +0530

    datetime module

---
+++ module-example.py
@@ -0,0 +1,12 @@
+import datetime
+datetime_object = datetime.datetime.now()
+print(datetime_object)
+
+date=datetime.date.today()
+print(date)
+
+from datetime import date
+today = date.today()
+print("current year:", today.year)
+print("current month:", today.month)
+print("current day:", today.day)
