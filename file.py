81142f6b30814386902baf0602afad2436945b47
bagad-billa420 <priyaranjangiri006@gmail.com>
2026-04-21 12:34:08 +0530

    file opening in w,r and a mode

--- file.py
+++ file.py
@@ -1,3 +1,26 @@
+#read the prior content of the file
+f=open("demo.txt","r")
+print(f.read())
+f.close()
+
+#write to the file
+#it over write the prior content of the file
+f=open("demo.txt","w")
+f.write("this the first line\n")
+f.write("this the second line\n")
+f.close()
+
+#new content of the file
+f=open("demo.txt","r")
+print(f.read())
+f.close()
+
+#append to the file
+f=open("demo.txt","a")
+f.write("this the third line\n")
+f.close()
+
+#new content of the file
 f=open("demo.txt","r")
 print(f.read())
 f.close()
