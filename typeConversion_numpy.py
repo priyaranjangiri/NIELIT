dbe41281d37465ea127f9a2eaec8845d30cf9c79
bagad-billa420 <priyaranjangiri006@gmail.com>
2026-04-30 12:23:43 +0530

    numpy array type conversion

---
+++ typeConversion_numpy.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+# arr = np.array([1,2,3,4])
+arr = np.array([1,2,3,4], dtype=np.int32)
+# new_arr=arr.astype(float)
+# print(new_arr)
+# print(new_arr.dtype)
+print(arr)
+print(arr.dtype)
