67692f0ec45b4524e20de251740f71de67f8ad31
bagad-billa420 <priyaranjangiri006@gmail.com>
2026-05-05 11:36:21 +0530

    numpy operations

---
+++ random_num.py
@@ -0,0 +1,5 @@
+import numpy as np 
+np.random.seed(0) # for reproducibility
+print(np.random.rand(3)) 
+print(np.random.randint(1, 10, 5)) 
+print(np.random.normal(0, 1, 4)) 
---
+++ zeros_ones.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+np.zeros(3)
+np.ones((2,3))
+np.zeros((2,2,2))
+
+np.ones(4)
+np.ones((3,2))
+np.ones((2,2,2))
+
+#2d matrix with 1s in the diagonal and 0s elsewhere, not applicable for 1d or 3d 
+print(np.eye(3))
+
